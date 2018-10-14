ba299d6be82e4f9ec90aa121def328d21c369d2b
AlexanderSilvaB <alexander.lmx@outlook.com>
2018-10-14 13:32:40 -0300

    Scene switcher

---
+++ src/app/Mobile Robotics Simulator/Assets/Scripts/SceneSelector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadScene(string sceneName)
+	{
+		StartCoroutine(LoadAsync(sceneName));
+	}
+
+	private IEnumerator LoadAsync(string sceneName)
+	{
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+		while(!asyncLoad.isDone)
+		{
+			yield return null;
+		}
+	}
+}
