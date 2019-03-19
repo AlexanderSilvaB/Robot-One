208eac512389c2fc0524709c2b0cdb1aa0cc03bb
AlexanderSilvaB <alexander.lmx@outlook.com>
2019-03-18 22:06:42 -0300

    New ControlP python example

--- src/examples/python/controlP.py
+++ src/examples/python/controlP.py
@@ -1,53 +1,45 @@
 import robotOne as ro
 import math as m
-from random import uniform
 
-handler = ro.connect("127.0.0.1")
-ro.trace(handler, True)
-ro.odometryStd(handler, [0.01, 0.01])
+def distance(P1, P2):
+    return m.sqrt(((P1[0] - P2[0]) ** 2) + ((P1[1] - P2[1]) ** 2))
+
+h = ro.connect("127.0.0.1")
+ro.trace(h, True)
+
+V = 5.0
 
 Kp = 0.1818181818181818
 Ka = 0.4191980558930741
 Kb = -0.1818181818181818
 
-MaxV = 5.0
-MaxD = 2.0
-#Square = 490
-Square = 50
+PS = [   [0, 0, 0], 
+        [8.70, 0.15, -0.11],
+        [32.70, -0.10, 0.05],
+        [54.10, -1.78, -0.81],
+        [53.69, -54.03, -1.74],
+        [54.10, -1.78, 2.33],
+        [32.70, -0.10, 3.09],
+        [8.70, 0.15, 3.03],
+        [0, 0, m.pi]]
 
-try:
-    GL = [[0, 0, 0], [0, 20, 0], [20, 20, 0], [20, 0, 0], [0, 0, 0]]
-    i = 0
-    while True:
-        G = GL[i]
-        print(G)
-        while True:
-            R = ro.odometry(handler)
-            dx = G[0] - R[0]
-            dy = G[1] - R[1]
-            p = m.sqrt(dx*dx + dy*dy)
-            #print(p)
-            if p > MaxD:
-                gamma = m.atan2(dy, dx)
-                alpha = ro.fixAngle(gamma - R[2])
-                beta = ro.fixAngle(G[2] - gamma)
-                v = min(Kp * p, MaxV)
-                w = Ka * alpha + Kb * beta
-                ro.velocity(handler, (v, w))
-            else:
-                break
-            ro.wait(handler)
-        i = i + 1
-        if i == len(GL):
-            break
+for i in range(len(PS)):
+    P = PS[i]
+    R = ro.pose(h)
+    p = distance(P, R)
+    while p > 1:
+        gamma = m.atan2(P[1] - R[1], P[0] - R[0])
+        alpha = ro.fixAngle(gamma - R[2])
+        beta = ro.fixAngle(P[2] - gamma)
+        w = Ka * alpha + Kb * beta
+        v = min(Kp * p, V)
+        ro.velocity(h, [v, w])
+        ro.wait(h)
+        R = ro.pose(h)
+        p = distance(P, R)
+        print("Point: %d, Distance: %f, Angular: %f" % (i, p, w))
 
-    ro.velocity(handler, (0, 0))
+ro.velocity(h, [0, 0])
+ro.wait(h)
 
-    R = ro.pose(handler)
-    print(G)
-    print(R)
-except Exception, e:
-    print(e)
-
-ro.trace(handler, False)
-ro.disconnect(handler)
+ro.disconnect(h)
