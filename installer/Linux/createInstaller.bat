0466111eb7cf76110280122002c1ac8ad1b3f169
AlexanderSilvaB <alexander.lmx@outlook.com>
2019-07-20 23:23:19 -0300

    Linux release

--- installer/Linux/createInstaller.bat
+++ installer/Linux/createInstaller.bat
@@ -1,6 +1,23 @@
 @echo off
 
 echo Robot One Linux Installer Creator
-md data
-xcopy.exe "../../src/lib" "data" /y
-xcopy.exe "../../src/examples" "data" /y
+md data\lib
+md data\examples
+md data\app
+xcopy.exe "../../src/lib" "data/lib" /y /i /s /e
+xcopy.exe "../../src/examples" "data/examples" /y /i /s /e
+copy /Y "..\..\softwares\installLinux.sh" "data\install.sh"
+xcopy.exe "../../build/Linux" "data/app" /y /i /s /e
+
+rmdir /s /q data\lib\c_cpp\build
+rmdir /s /q data\lib\c_cpp\build-linux
+rmdir /s /q data\examples\cpp\build
+rmdir /s /q data\examples\cpp\build-linux
+
+copy /Y "data\lib\c_cpp\robotOne_matlab.h" "data\lib\matlab\robotOne\robotOne.h"
+
+cd data
+"../../../softwares/zip.exe" -r ../robotOne.zip *
+cd ..
+
+rmdir /s /q data
--- softwares/installLinux.sh
+++ softwares/installLinux.sh
@@ -3,7 +3,6 @@
 echo "Robot-One Linux Installer"
 
 
-cd ../../src
 echo "Compiling C/C++ Library..."
 cd lib/c_cpp
 mkdir -p build-linux
@@ -18,19 +17,37 @@ echo "Installing Python Library..."
 mkdir -p ~/robot-one/lib/python
 cp lib/python/robotOne.py ~/robot-one/lib/python/robotOne.py
 
-#echo "Installing Matlab Library..."
-#mkdir -p ~/robot-one/lib/matlab
-#cp -r lib/matlab/robotOne ~/robot-one/lib/matlab/
+echo "Installing Matlab Library..."
+mkdir -p ~/robot-one/lib/matlab
+cp -r lib/matlab/robotOne ~/robot-one/lib/matlab/
 
 echo "Installing examples..."
 mkdir -p ~/robot-one/examples
 cp -r examples ~/robot-one/
 
-echo "Hint: The matlab examples still do not work out-of-the-box"
+echo "Installing application..."
+mkdir -p ~/robot-one/app
+cp -r app ~/robot-one/
 
-echo "Setting Python path"
-echo "export PYTHONPATH=\"${PYTHONPATH}:${HOME}/robot-one/lib/python\"" >> ~/.bashrc
+echo "Setting Python path..."
+add_python_path () {
+        if ! echo "$PYTHONPATH" | /bin/grep -Eq "(^|:)$1($|:)" ; then
+           echo "export PYTHONPATH=\"${PYTHONPATH}:$1\"" >> ~/.bashrc
+           if [ "$2" = "after" ] ; then
+              PYTHONPATH="$PYTHONPATH:$1"
+           else
+              PYTHONPATH="$1:$PYTHONPATH"
+           fi
+        fi
+}
 
-cd ../installer/Linux
+add_python_path ${HOME}/robot-one/lib/python
+
+echo "Configuring application..."
+chmod +x ~/robot-one/app/robotOne.x86_64
+
+echo "cd ${HOME}/robot-one/app; ./robotOne.x86_64; cd ${PWD}" >> ~/robot-one/app/robotOne
+chmod +x ~/robot-one/app/robotOne
+sudo ln -sf ~/robot-one/app/robotOne /usr/local/bin/robotOne
 
 echo "Done!"
