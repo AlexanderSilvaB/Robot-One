72d183a5be6e8afbcf66e0c94f6cd278bc8542c9
AlexanderSilvaB <alexander.lmx@outlook.com>
2019-07-20 23:30:39 -0300

    Linux release

--- installer/Linux/createInstaller.bat
+++ installer/Linux/createInstaller.bat
@@ -1,23 +1,23 @@
 @echo off
 
 echo Robot One Linux Installer Creator
-md data\lib
-md data\examples
-md data\app
-xcopy.exe "../../src/lib" "data/lib" /y /i /s /e
-xcopy.exe "../../src/examples" "data/examples" /y /i /s /e
-copy /Y "..\..\softwares\installLinux.sh" "data\install.sh"
-xcopy.exe "../../build/Linux" "data/app" /y /i /s /e
+md robotOne-linux-x86_64\robotOne-linux-x86_64\lib
+md robotOne-linux-x86_64\robotOne-linux-x86_64\examples
+md robotOne-linux-x86_64\robotOne-linux-x86_64\app
+xcopy.exe "../../src/lib" "robotOne-linux-x86_64/robotOne-linux-x86_64/lib" /y /i /s /e
+xcopy.exe "../../src/examples" "robotOne-linux-x86_64/robotOne-linux-x86_64/examples" /y /i /s /e
+copy /Y "..\..\softwares\installLinux.sh" "robotOne-linux-x86_64\robotOne-linux-x86_64\install.sh"
+xcopy.exe "../../build/Linux" "robotOne-linux-x86_64/robotOne-linux-x86_64/app" /y /i /s /e
 
-rmdir /s /q data\lib\c_cpp\build
-rmdir /s /q data\lib\c_cpp\build-linux
-rmdir /s /q data\examples\cpp\build
-rmdir /s /q data\examples\cpp\build-linux
+rmdir /s /q robotOne-linux-x86_64\robotOne-linux-x86_64\lib\c_cpp\build
+rmdir /s /q robotOne-linux-x86_64\robotOne-linux-x86_64\lib\c_cpp\build-linux
+rmdir /s /q robotOne-linux-x86_64\robotOne-linux-x86_64\examples\cpp\build
+rmdir /s /q robotOne-linux-x86_64\robotOne-linux-x86_64\examples\cpp\build-linux
 
-copy /Y "data\lib\c_cpp\robotOne_matlab.h" "data\lib\matlab\robotOne\robotOne.h"
+copy /Y "robotOne-linux-x86_64\robotOne-linux-x86_64\lib\c_cpp\robotOne_matlab.h" "robotOne-linux-x86_64\robotOne-linux-x86_64\lib\matlab\robotOne\robotOne.h"
 
-cd data
-"../../../softwares/zip.exe" -r ../robotOne.zip *
+cd robotOne-linux-x86_64
+"../../../softwares/zip.exe" -r ../robotOne-linux-x86_64.zip *
 cd ..
 
-rmdir /s /q data
+rmdir /s /q robotOne-linux-x86_64
